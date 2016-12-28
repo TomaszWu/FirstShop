d9adc6219f8f9bbd526ab95e8d348ed3a221c083
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-12-28 23:34:00 +0100

    17

--- index.php
+++ index.php
@@ -23,215 +23,72 @@ $allCategies = Category::getAllCategories($conn);
                 echo
                 $singleCategory->getCategoryName();
                 $categoryId = $singleCategory->getCategoryId();
-                $mainPagePhotos = Category::getPhotoForMainPageForOneCategory($conn, $categoryId)
+                $mainPagePhotos = Picture::getPhotoForMainPageForOneCategory($conn, $categoryId)
                 ?></h1>
             <div id="myCarouse<?php echo $i ?>" class="carousel slide" data-ride="carouse<?php echo $i ?>">
-                <!-- Indicators -->
-                <!--                <ol class="carousel-indicators">
-                                    <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="0" class="active"></li>
-                                    <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="1"></li>
-                                    <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="2"></li>
-                                    <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="3"></li>
-                                </ol>-->
+
 
                 <ol class="carousel-indicators">
                     <?php
-                    for ($j = 0; $j < count($MainPagePhotos); $j++) {
+                    for ($j = 0; $j < count($mainPagePhotos); $j++) {
                         if ($j == 0) {
                             ?>
-                            <div class="item active">
-                                <img src="<?php echo $mainPagePhotos[$j]->getPicture_link(); ?>" alt="Chania">
-                            </div>
+                            <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="<?php echo $j ?>" class="active"></li>
                         <?php } else {
                             ?>
-                            <div class="item">
-                                <img src="<?php $mainPagePhotos->getPicture_link(); ?>" alt="Chania">
-                            </div>
-                        </ol>
-                        <?php
+                            <li data-target="#myCarouse<?php echo $i ?>" data-slide-to="<?php echo $j ?>"></li>
+
+                            <?php
+                        }
                     }
-                }
-                ?>
+                    ?>
+                </ol>
+
+
+
                 <!--Wrapper for slides -->
                 <div class = "carousel-inner" role = "listbox">
+                    <?php
+                    for ($j = 0; $j < count($mainPagePhotos); $j++) {
+                        if ($j == 0) {
+                            ?>
+                            <div class = "item active">
+                                <a href = "showCategory.php?id=<?php echo $i ?>"><img src = "<?php echo $mainPagePhotos[$j]->getPicture_link(); ?>" alt = "Chania" width = "460" height = "345"></a>
+                                <div class = "carousel-caption">
+                                    <h3><?php
+                                        echo
+                                        $singleCategory->getCategoryName()
+                                        ?></h3>
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                </div>
+                            </div>
+        <?php } else { ?>
+                            <div class="item">
+                                <a href="showCategory.php?id=<?php echo $i ?>"><img src="<?php echo $mainPagePhotos[$j]->getPicture_link(); ?>" alt="Chania" width="460" height="345"></a>
+                                <div class="carousel-caption">
+                                    <h3>Samochody</h3>
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                </div>
+                            </div>
+                        <?php
+                        }
+                    }
+                    ?>
 
-                    <div class = "item active">
-                        <a href = "showCategory.php?id=<?php echo $i ?>"><img src = "/FirstShop/Pictures/1/29.jpg" alt = "Chania" width = "460" height = "345"></a>
-                        <div class = "carousel-caption">
-                            <h3><?php
-                                echo
-                                $singleCategory->getCategoryName()
-                                ?></h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="showCategory.php?id=<?php echo $i ?>"><<img src="/FirstShop/Pictures/1/29_1.jpg" alt="Chania" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Samochody</h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="showCategory.php?id=<?php echo $i ?>"><<img src="/FirstShop/Pictures/31/31.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Samochody</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="showCategory.php?id=<?php echo $i ?>"><<img src="/FirstShop/Pictures/33/33_1.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Samochody</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarouse<?php echo $i ?>" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarouse<?php echo $i ?>" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarouse<?php echo $i ?>" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarouse<?php echo $i ?>" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
-        </div>
-        <?php
-        $i++;
-    }
-    ?>
-    <!--    <div id="section2" class="container-fluid">
-            <h1>Kwiaty</h1>
-            <div id="myCarouse2" class="carousel slide" data-ride="carousel">
-                 Indicators 
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarouse2" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarouse2" data-slide-to="1"></li>
-                    <li data-target="#myCarouse2" data-slide-to="2"></li>
-                    <li data-target="#myCarouse2" data-slide-to="3"></li>
-                </ol>
-    
-                 Wrapper for slides 
-                <div class="carousel-inner" role="listbox">
-    
-                    <div class="item active">
-                        <a href="showCategory.php?id=2"><img src="/FirstShop/Pictures/19/19.jpg" alt="Chania" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Chania</h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=2"><img src="/FirstShop/Pictures/26/26.jpg" alt="Chania" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Chania</h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=2"><img src="/FirstShop/Pictures/34/34.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Flowers</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=2"><img src="/FirstShop/Pictures/35/35.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Flowers</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-    
-                </div>
-    
-                 Left and right controls 
-                <a class="left carousel-control" href="#myCarouse2" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarouse2" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </div>
-    
-        <div>
-            <p>Try to scroll this section and look at the navigation bar whiyle scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        <p>Tr to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section3" class="container-fluid">
-            <h1>Narkotyki</h1>
-            <div id="myCarouse3" class="carousel slide" data-ride="carousel">
-                 Indicators 
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarouse3" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarouse3" data-slide-to="1"></li>
-                    <li data-target="#myCarouse3" data-slide-to="2"></li>
-                    <li data-target="#myCarouse3" data-slide-to="3"></li>
-                </ol>
-    
-                 Wrapper for slides 
-                <div class="carousel-inner" role="listbox">
-    
-                    <div class="item active">
-                        <a href="showCategory.php?id=3"><img src="Pictures/21/21_1.jpg" alt="Chania" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Chania</h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=3"><img src="Pictures/22/22.jpg" alt="Chania" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Chania</h3>
-                            <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=3"><img src="Pictures/23/23.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Flowers</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-    
-                    <div class="item">
-                        <a href="showCategory.php?id=3"><img src="Pictures/24/24.jpg" alt="Flower" width="460" height="345"></a>
-                        <div class="carousel-caption">
-                            <h3>Flowers</h3>
-                            <p>Beatiful flowers in Kolymbari, Crete.</p>
-                        </div>
-                    </div>
-    
-                </div>
-    
-                 Left and right controls 
-                <a class="left carousel-control" href="#myCarouse2" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarouse2" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </div>-->
-
-
-
+            <?php
+            $i++;
+        }
+        ?>
 </body>
 </html>
