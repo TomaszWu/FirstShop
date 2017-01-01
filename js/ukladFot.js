f696c094125d08b115dbe0f473925a7b1df7ce89
TomaszWu <tomasz.wojciechowski84@gmail.com>
2017-01-01 21:32:10 +0100

    uklad

--- js/ukladFot.js
+++ js/ukladFot.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-    console.log('tak');
-    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
-    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
-});
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
