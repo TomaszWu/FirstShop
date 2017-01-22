d768c44376360392529bf0a772bbcf95298ea2fe
TomaszWu <tomasz.wojciechowski84@gmail.com>
2017-01-22 13:35:42 +0100

    several var_dumps removed

--- pictures.php
+++ pictures.php
@@ -3,14 +3,11 @@ session_start();
 include(__DIR__ . '/src/Db.php');
 require_once __DIR__ . '/vendor/autoload.php';
 $conn = DB::connect();
-var_dump($_POST);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    var_dump($_FILES);
     $allowedFileTypes = ['image/png', 'image/jpeg', 'image/gif'];
     if (isset($_POST['pictureId']) && isset($_FILES['fileToUpload']) &&
             $_FILES['fileToUpload']['size'] > 0 && in_array($_FILES['fileToUpload']['type'], $allowedFileTypes)) {
         $productId = $_POST['pictureId'];
-        var_dump($productId);
         $uploadDir = 'Pictures';
 
         if (is_dir($uploadDir . '/' . $productId)) {
@@ -23,7 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $destination = $uploadDir;
         $destinationFileName = $destination . '/' . $productId . '/' . $_FILES['fileToUpload']['name'];
-        var_dump(quotemeta($destinationFileName));
         $query = "INSERT INTO Pictures (picture_link, product_id) VALUES ('" . quotemeta($destinationFileName) . "', '" . $productId . "')";
 
         if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $destinationFileName)) {
